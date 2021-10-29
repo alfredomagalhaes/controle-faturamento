12e3f71e35e87e8337706a44c39a06f2ee461798
Alfredo Magalhães <alfredo.magalhaes@outlook.com>
2021-10-29 01:01:35 -0300

    Inicio de criação de aplicação de front end em React

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Routes from "./routes";
+import "./styles/global";
+import "antd/dist/antd.css";
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
 
 const App = () => <Routes />;
 export default App;
---
+++ webui/src/pages/SignIn/index.js
@@ -0,0 +1,121 @@
+/*!
+=========================================================
+* Muse Ant Design Dashboard - v1.0.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
+* Coded by Creative Tim
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+import React, { Component } from "react";
+import {
+  Layout,
+  Button,
+  Row,
+  Col,
+  Typography,
+  Form,
+  Input,
+  Switch,
+} from "antd";
+
+function onChange(checked) {
+  console.log(`switch to ${checked}`);
+}
+const { Title } = Typography;
+const { Header, Content } = Layout;
+
+export default class SignIn extends Component {
+  render() {
+    const onFinish = (values) => {
+      console.log("Success:", values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
+    };
+    return (
+      <>
+        <Layout className="layout-default layout-signin">
+          <Header>
+            <div className="header-col header-brand">
+              <h5>Controle financeiro</h5>
+            </div>
+
+          </Header>
+          <Content className="signin">
+            <Row gutter={[24, 0]} justify="space-around">
+              <Col
+                xs={{ span: 24, offset: 0 }}
+                lg={{ span: 6, offset: 2 }}
+                md={{ span: 12 }}
+              >
+                <Title className="mb-15">Sign In</Title>
+                <Title className="font-regular text-muted" level={5}>
+                  Enter your email and password to sign in
+                </Title>
+                <Form
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}
+                  layout="vertical"
+                  className="row-col"
+                >
+                  <Form.Item
+                    className="username"
+                    label="Email"
+                    name="email"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your email!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Email" />
+                  </Form.Item>
+
+                  <Form.Item
+                    className="username"
+                    label="Password"
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your password!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Password" />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="remember"
+                    className="aligin-center"
+                    valuePropName="checked"
+                  >
+                    <Switch defaultChecked onChange={onChange} />
+                    Remember me
+                  </Form.Item>
+
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      style={{ width: "100%" }}
+                      to="/home"
+                    >
+                      SIGN IN
+                    </Button>
+                  </Form.Item>
+                  
+                </Form>
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+      </>
+    );
+  }
+}
